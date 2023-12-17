b83ef146f74b99c1fe1052df42e9016745ada76e
HarutAzatyan777 <harutazatyan45@gmail.com>
2023-12-18 01:07:50 +0400

    18.12.2023

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+// Header.jsx
 
-function Header() {
+import React from 'react';
+import './Header.css'; // Import your CSS file for styling
+
+const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="main-header">
+      <div className="logo-container">
+        <img src="/path/to/logo.png" alt="Logo" className="logo" />
+      </div>
+      <nav className="main-navigation">
+        <ul>
+          <li><a href="#">Products</a></li>
+          <li className="dropdown">
+            <a href="#">Features</a>
+            <div className="dropdown-content">
+              {/* Dropdown menu items */}
+              <a href="#">Feature 1</a>
+              <a href="#">Feature 2</a>
+              <a href="#">Feature 3</a>
+            </div>
+          </li>
+          <li><a href="#">Resources</a></li>
+          <li><a href="#">Plans & Pricing</a></li>
+          <li><a href="#">Log In</a></li>
+          <li><a href="#">Get Started</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import "./Home.css"
+// Home.jsx
+
+import React from 'react';
+import './Home.css';
+import Header from '../../components/Header/Header';
+import Gallery from '../../components/Header/Gallery/Gallery';
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <Header />
+
+      <section className="landing-section">
+        <div className="landing-content">
+          <h1>Welcome to Our Agency</h1>
+          <p>
+            We are a team of freelance web developers, ready to bring your ideas to life.
+            Whether you need a personal website or a complex business solution, we've got you covered.
+          </p>
+          <p>
+            Our expertise extends to working with in-house teams within businesses.
+            Let's collaborate to create innovative and impactful web solutions.
+          </p>
+          <button className="cta-button">Get Started</button>
+        </div>
+      </section>
+<Gallery />
+   
+    </div>
+  );
 }
 
-export default Home
+export default Home;
