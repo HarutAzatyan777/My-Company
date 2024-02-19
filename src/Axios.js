68047141299839ff459978f38ab79b668bf90faa
HarutAzatyan777 <harutazatyan45@gmail.com>
2024-02-19 23:09:04 +0400

    19.02.2024-2

--- src/Axios.js
+++ src/Axios.js
@@ -1,36 +1,16 @@
-import React, { useState } from 'react';
-import instance from './Axios'; // Import the Axios instance
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://api.harutdev.com/',
+});
+
+instance.interceptors.request.use((config) => {
+  config.headers.Authorization = window.localStorage.getItem('token');
+  return config;
+});
+
+export default instance;
 
 
-
-const ChatComponent = () => {
-  const [messageInput, setMessageInput] = useState('');
-
-  const sendMessage = async () => {
-    try {
-      // Make a POST request using the Axios instance
-      await instance.post('/socket-io-data', { data: messageInput });
-
-      // Optionally, update the local state or perform any other actions
-      console.log('Message sent successfully');
-    } catch (error) {
-      console.error('Error sending message:', error);
-      // Handle the error (e.g., display a message to the user)
-    }
-
-    setMessageInput(''); // Clear the input field after sending the message
-  };
-
-  return (
-    <div>
-      <input
-        type="text"
-        value={messageInput}
-        onChange={(e) => setMessageInput(e.target.value)}
-      />
-      <button onClick={sendMessage}>Send</button>
-    </div>
-  );
-};
-
-export default ChatComponent;
+ // baseURL: ' http://localhost:5000/',
+//  baseURL: 'https://api.harutdev.com/',
